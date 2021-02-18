4497712201a84aca00d87beaf5796910f7f950c1
ThansiJamal <thansijamal18@gmail.com>
2021-02-17 23:28:21 -0800

    first commit

---
+++ AccessSpecifierConsole/Account.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpecifierConsole
+{
+   public class Account
+    {
+        public string Withdraw { get; set; }
+        public string Deposit { get; set; }
+       
+}
---
+++ AccessSpecifierConsole/SavingsAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessSpecifierConsole
+{
+   public class SavingsAccount:Account
+    {
+        public void setSavingsAmount()
+        {
+           Console.WriteLine("Savings amount set to $Y.");
+        }
+}
